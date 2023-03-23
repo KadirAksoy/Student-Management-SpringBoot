a15337db3fc2db1ad327daddce58d3452a73fa7d
Kadir Baran Aksoy <kadirbaranaksoy@icloud.com>
2023-03-23 11:41:20 +0300

    some method of controller added.

--- src/main/java/com/kadiraksoy/StudentManagement/controller/StudentController.java
+++ src/main/java/com/kadiraksoy/StudentManagement/controller/StudentController.java
@@ -1,11 +1,12 @@
 package com.kadiraksoy.StudentManagement.controller;
 
 
+import com.kadiraksoy.StudentManagement.dto.StudentDto;
 import com.kadiraksoy.StudentManagement.model.Student;
 import com.kadiraksoy.StudentManagement.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class StudentController {
@@ -24,6 +25,7 @@ public class StudentController {
         return "students";
     }
 
+    @GetMapping("/students/new")
     public String createStudent(Model model){
         Student student = new Student();
 
@@ -31,6 +33,42 @@ public class StudentController {
 
         return "create_student";
     }
+
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute ("student") StudentDto studentDto){
+        studentService.save(studentDto);
+        return "redirect:/students";
+    }
+
+    @DeleteMapping("/students/{id}")
+    public String deleteStudent(@PathVariable Long id){
+        studentService.deleteStudentById(id);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/edit/{id}")
+    public String editStudentForm(@PathVariable Long id, Model model) {
+        model.addAttribute("student", studentService.getStudentById(id));
+        return "edit_student";
+    }
+
+    @PostMapping("/students/{id}")
+    public String updateStudent(@PathVariable Long id,
+                                @ModelAttribute("student") Student student,
+                                Model model) {
+
+        // get student from database by id
+        StudentDto existingStudent = studentService.getStudentById(id);
+        existingStudent.setName(student.getName());
+        existingStudent.setLastName(student.getLastName());
+        existingStudent.setEmail(student.getEmail());
+
+        // save updated student object
+        studentService.updateStudent(existingStudent);
+        return "redirect:/students";
+    }
+
+
 
 
 }
--- src/main/java/com/kadiraksoy/StudentManagement/dto/StudentDto.java
+++ src/main/java/com/kadiraksoy/StudentManagement/dto/StudentDto.java
@@ -1,13 +1,13 @@
 package com.kadiraksoy.StudentManagement.dto;
 
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudentDto {
 
     private String name;
