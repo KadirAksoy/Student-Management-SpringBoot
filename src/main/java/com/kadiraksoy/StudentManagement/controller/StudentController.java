3359f6a27be6361eb60fd2788513c1efe935023e
Kadir Baran Aksoy <kadirbaranaksoy@icloud.com>
2023-03-23 10:40:24 +0300

    two method of controller added.

--- src/main/java/com/kadiraksoy/StudentManagement/controller/StudentController.java
+++ src/main/java/com/kadiraksoy/StudentManagement/controller/StudentController.java
@@ -1,6 +1,36 @@
 package com.kadiraksoy.StudentManagement.controller;
 
 
+import com.kadiraksoy.StudentManagement.model.Student;
+import com.kadiraksoy.StudentManagement.service.StudentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+
+
+    }
+
+    @GetMapping("/students")
+    public String listStundets(Model model){
+        model.addAttribute("students",studentService.getAllStudent());
+        return "students";
+    }
+
+    public String createStudent(Model model){
+        Student student = new Student();
+
+        model.addAttribute("student",student);
+
+        return "create_student";
+    }
+
+
 }
