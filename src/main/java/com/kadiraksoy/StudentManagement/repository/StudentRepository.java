274e598e9de8882b3dfa118b525d907db96c88ca
Kadir Baran Aksoy <kadirbaranaksoy@icloud.com>
2023-03-26 15:16:21 +0300

    Repository.

--- src/main/java/com/kadiraksoy/StudentManagement/repository/StudentRepository.java
+++ src/main/java/com/kadiraksoy/StudentManagement/repository/StudentRepository.java
@@ -2,6 +2,9 @@ package com.kadiraksoy.StudentManagement.repository;
 
 import com.kadiraksoy.StudentManagement.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
